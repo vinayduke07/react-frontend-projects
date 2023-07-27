7a5b36df7c25cd5d14cd1cc1d757b6e642725c9c
vinayduke07 <58109933+vinayduke07@users.noreply.github.com>
2023-07-27 19:23:51 +0530

    this is my best commit

---
+++ todo-list-app/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors:{
+      'blue': '#646af9',
+      'lightBlue': '#5f4ac9',
+      'dark-blue': '#0087d1'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
